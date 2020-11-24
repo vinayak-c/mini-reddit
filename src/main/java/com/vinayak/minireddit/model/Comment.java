cdfd3db8cebe2fe7d51f51e17192c75388f64268
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 15:58:28 +0530

    Added required annotations

--- src/main/java/com/vinayak/minireddit/model/Comment.java
+++ src/main/java/com/vinayak/minireddit/model/Comment.java
@@ -1,9 +1,17 @@
 package com.vinayak.minireddit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Comment {
 
     @Id
