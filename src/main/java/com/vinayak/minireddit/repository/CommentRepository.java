cfc92d921a5a4dc6b53ebc86e0e62a9713202963
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 14:35:45 +0530

    Added Comment Repository

---
+++ src/main/java/com/vinayak/minireddit/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.vinayak.minireddit.repository;
+
+import com.vinayak.minireddit.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
