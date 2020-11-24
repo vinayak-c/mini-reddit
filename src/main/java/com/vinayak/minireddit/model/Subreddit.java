3cb7ea16a5b31d76656c638f69fb1a098819fb8b
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 13:51:56 +0530

    Added User Subreddit model

---
+++ src/main/java/com/vinayak/minireddit/model/Subreddit.java
@@ -0,0 +1,27 @@
+package com.vinayak.minireddit.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
+public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Community name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
