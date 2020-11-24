36a8c5f36320afeedc85cb137813fb4aa8b35853
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 14:36:52 +0530

    Added Verification Token  Repository

---
+++ src/main/java/com/vinayak/minireddit/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.vinayak.minireddit.repository;
+
+import com.vinayak.minireddit.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Long> {
+}
