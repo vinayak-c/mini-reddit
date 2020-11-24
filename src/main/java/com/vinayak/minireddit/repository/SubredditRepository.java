2a437e4c6592a8141d5fe5f278f74914c522433a
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 14:36:12 +0530

    Added Subreddit Repository

---
+++ src/main/java/com/vinayak/minireddit/repository/SubredditRepository.java
@@ -0,0 +1,9 @@
+package com.vinayak.minireddit.repository;
+
+import com.vinayak.minireddit.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit,Long> {
+}
