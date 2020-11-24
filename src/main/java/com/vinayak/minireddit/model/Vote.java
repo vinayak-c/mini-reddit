1d140e4382f9f113d6d6f2291b4a3d9dd0ddf09b
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 13:54:10 +0530

    Added Vote model

---
+++ src/main/java/com/vinayak/minireddit/model/Vote.java
@@ -0,0 +1,23 @@
+package com.vinayak.minireddit.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+
+    private VoteType voteType;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+}
+
