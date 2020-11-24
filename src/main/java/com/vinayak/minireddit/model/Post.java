f51b1472e5b1a71a2e7c1ac35d19026f7bcb206a
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 13:52:20 +0530

    Added Post  model

---
+++ src/main/java/com/vinayak/minireddit/model/Post.java
@@ -0,0 +1,48 @@
+package com.vinayak.minireddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import java.time.Instant;
+
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long postId;
+
+    @NotBlank(message="Post name cannot be empty or null")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    private Integer voteCount=0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
+}
