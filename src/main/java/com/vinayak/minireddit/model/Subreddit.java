5026ca7191f7e94c649b53d66c8a0baefbb485f2
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 15:58:50 +0530

    Added required annotations

--- src/main/java/com/vinayak/minireddit/model/Subreddit.java
+++ src/main/java/com/vinayak/minireddit/model/Subreddit.java
@@ -1,10 +1,20 @@
 package com.vinayak.minireddit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Subreddit {
 
     @Id
