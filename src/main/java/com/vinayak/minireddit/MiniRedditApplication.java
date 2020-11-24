a78bece49f5bd363978ae2c68aef68ee77965fb4
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-24 12:07:46 +0530

    Initial commit

---
+++ src/main/java/com/vinayak/minireddit/MiniRedditApplication.java
@@ -0,0 +1,13 @@
+package com.vinayak.minireddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinayak/minireddit/MiniRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.minireddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
