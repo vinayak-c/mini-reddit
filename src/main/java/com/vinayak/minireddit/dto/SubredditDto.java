959c63e7536dccd835df4a908a41f0337d876baf
Vinayak <vinayakchaurasia99@gmail.com>
2020-11-30 17:22:07 +0530

    Added SubredditDto.java

---
+++ src/main/java/com/vinayak/minireddit/dto/SubredditDto.java
@@ -0,0 +1,19 @@
+package com.vinayak.minireddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SubredditDto {
+
+    private Long id;
+    private String subredditName;
+    private String description;
+    private Integer numberOfPosts;
+
+}
