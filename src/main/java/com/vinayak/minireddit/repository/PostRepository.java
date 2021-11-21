cb26caad2e2f19b91d4e0a9504de020fd106e7e3
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-22 00:16:39 +0530

    Added methods for Post's service

--- src/main/java/com/vinayak/minireddit/repository/PostRepository.java
+++ src/main/java/com/vinayak/minireddit/repository/PostRepository.java
@@ -1,9 +1,17 @@
 package com.vinayak.minireddit.repository;
 
 import com.vinayak.minireddit.model.Post;
+import com.vinayak.minireddit.model.Subreddit;
+import com.vinayak.minireddit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+
+    List<Post> findByUser(User user);
 }
