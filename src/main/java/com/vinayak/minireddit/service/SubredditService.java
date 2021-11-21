bb16a61998b34643a11be75e3fe4e2863d176f79
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-21 18:24:47 +0530

    Updated service to use MapStruct methods

--- src/main/java/com/vinayak/minireddit/service/SubredditService.java
+++ src/main/java/com/vinayak/minireddit/service/SubredditService.java
@@ -1,6 +1,8 @@
 package com.vinayak.minireddit.service;
 
 import com.vinayak.minireddit.dto.SubredditDto;
+import com.vinayak.minireddit.exceptions.SpringRedditException;
+import com.vinayak.minireddit.mapper.SubredditMapper;
 import com.vinayak.minireddit.model.Subreddit;
 import com.vinayak.minireddit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -18,10 +20,11 @@ public class SubredditService {
 
 
     private final SubredditRepository subredditRepository;
+    private final SubredditMapper subredditMapper;
 
     @Transactional
     public SubredditDto save(SubredditDto subredditDto){
-        Subreddit save=subredditRepository.save(mapSubredditDto(subredditDto));
+        Subreddit save=subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
         subredditDto.setId(save.getId());
         return subredditDto;
     }
@@ -30,23 +33,29 @@ public class SubredditService {
     public List<SubredditDto> getAll() {
           return subredditRepository.findAll()
                   .stream()
-                  .map(this::mapToDto)
+                  .map(subredditMapper::mapSubredditToDto)
                   .collect(Collectors.toList());
     }
 
-    private SubredditDto mapToDto(Subreddit subreddit) {
-        return SubredditDto.builder().subredditName(subreddit.getName())
-                .description(subreddit.getDescription())
-                .id(subreddit.getId())
-                .numberOfPosts(subreddit.getPosts().size())
-                .build();
+    public SubredditDto getSubreddit(Long id) {
+        Subreddit subreddit = subredditRepository.findById(id)
+                .orElseThrow(() -> new SpringRedditException("No subreddit found with ID - " + id));
+        return subredditMapper.mapSubredditToDto(subreddit);
     }
 
-    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
-        return Subreddit.builder().name(subredditDto.getSubredditName())
-                .description(subredditDto.getDescription())
-                .build();
-    }
+//    private SubredditDto mapToDto(Subreddit subreddit) {
+//        return SubredditDto.builder().name(subreddit.getName())
+//                .description(subreddit.getDescription())
+//                .id(subreddit.getId())
+//                .numberOfPosts(subreddit.getPosts().size())
+//                .build();
+//    }
+
+//    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
+//        return Subreddit.builder().name(subredditDto.getName())
+//                .description(subredditDto.getDescription())
+//                .build();
+//    }
 
 
 }
