1f30e896d236e6b815e4f234ca2e6c05c0ff7733
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-21 18:17:01 +0530

    Optimised imports

--- src/main/java/com/vinayak/minireddit/service/UserDetailsServiceImpl.java
+++ src/main/java/com/vinayak/minireddit/service/UserDetailsServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
