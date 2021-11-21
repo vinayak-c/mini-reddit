aa6dbaaa9c6c1136f0c627dc4ca8401c527abbd6
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-21 18:25:40 +0530

    Added endpoint to fetch subreddit by ID

--- src/main/java/com/vinayak/minireddit/controller/SubredditController.java
+++ src/main/java/com/vinayak/minireddit/controller/SubredditController.java
@@ -31,6 +31,12 @@ public class SubredditController {
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<SubredditDto> getSubreddit(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(subredditService.getSubreddit(id));
+    }
 
 
 }
