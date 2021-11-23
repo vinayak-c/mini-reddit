786b7ab7443057c25f3b5300377b1ff69f0a55da
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-23 21:47:01 +0530

    Added methods to fetch post by user and post

--- src/main/java/com/vinayak/minireddit/repository/CommentRepository.java
+++ src/main/java/com/vinayak/minireddit/repository/CommentRepository.java
@@ -1,9 +1,17 @@
 package com.vinayak.minireddit.repository;
 
 import com.vinayak.minireddit.model.Comment;
+import com.vinayak.minireddit.model.Post;
+import com.vinayak.minireddit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }
