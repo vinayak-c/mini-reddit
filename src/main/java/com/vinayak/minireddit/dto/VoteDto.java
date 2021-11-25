14e69913959d6c0c2f22974b9673301a1125a4a0
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-26 00:40:19 +0530

    Added Voting implementation

---
+++ src/main/java/com/vinayak/minireddit/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.vinayak.minireddit.dto;
+
+import com.vinayak.minireddit.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
---
+++ src/main/java/com/vinayak/minireddit/service/VoteService.java
@@ -0,0 +1,54 @@
+package com.vinayak.minireddit.service;
+
+import com.vinayak.minireddit.dto.VoteDto;
+import com.vinayak.minireddit.exceptions.PostNotFoundException;
+import com.vinayak.minireddit.exceptions.SpringRedditException;
+import com.vinayak.minireddit.model.Post;
+import com.vinayak.minireddit.model.Vote;
+import com.vinayak.minireddit.repository.PostRepository;
+import com.vinayak.minireddit.repository.VoteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.vinayak.minireddit.model.VoteType.UPVOTE;
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+
+    private final VoteRepository voteRepository;
+    private final PostRepository postRepository;
+    private final AuthService authService;
+
+    @Transactional
+    public void vote(VoteDto voteDto) {
+        Post post = postRepository.findById(voteDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException("Post Not Found with ID - " + voteDto.getPostId()));
+        Optional<Vote> voteByPostAndUser = voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post, authService.getCurrentUser());
+        if (voteByPostAndUser.isPresent() &&
+                voteByPostAndUser.get().getVoteType()
+                        .equals(voteDto.getVoteType())) {
+            throw new SpringRedditException("You have already "
+                    + voteDto.getVoteType() + "'d for this post");
+        }
+        if (UPVOTE.equals(voteDto.getVoteType())) {
+            post.setVoteCount(post.getVoteCount() + 1);
+        } else {
+            post.setVoteCount(post.getVoteCount() - 1);
+        }
+        voteRepository.save(mapToVote(voteDto, post));
+        postRepository.save(post);
+    }
+
+    private Vote mapToVote(VoteDto voteDto, Post post) {
+        return Vote.builder()
+                .voteType(voteDto.getVoteType())
+                .post(post)
+                .user(authService.getCurrentUser())
+                .build();
+    }
+
+}
