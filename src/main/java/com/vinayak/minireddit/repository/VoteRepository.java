1f5479378d79516518d451b6586fd158e9946b7b
Vinayak <vinayakchaurasia99@gmail.com>
2021-11-26 00:38:24 +0530

    Added method to fetch Post by user

--- src/main/java/com/vinayak/minireddit/repository/VoteRepository.java
+++ src/main/java/com/vinayak/minireddit/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.vinayak.minireddit.repository;
 
+import com.vinayak.minireddit.model.Post;
+import com.vinayak.minireddit.model.User;
 import com.vinayak.minireddit.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote,Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
